f3fa3115da7ad018ecaaa55220a9bc8397c6bed0
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-12-24 12:47:46 +0800

    timer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,23 @@ import './App.css'
 function App() {
 
   const [playStatus, setPlayStatus] = useState(false)
+  const [timer, setTimer] = useState(4)
+
+  useEffect(() => {
+    if (!playStatus) return
+
+    const intervalId = setInterval(() => {
+      setTimer(prev => {
+        if (prev <= 1) {
+          clearInterval(intervalId)
+          return 0
+        }
+        return prev - 1
+      })
+    }, 1000)
+
+    return () => clearInterval(intervalId)
+  }, [playStatus])
 
   return (
     <>
@@ -16,9 +33,13 @@ function App() {
       </header>
       <main>
 
-        { !playStatus && (
+        { !playStatus ? (
           <div id="start-container">
             <button id="start-btn" onClick={() => setPlayStatus(true)}>Start</button>
+          </div>
+        ) : (
+          <div id="timer-container">
+            <h1 id="timer">{ timer }</h1>
           </div>
         )}
 
